673d78b6bb0f6bd3015eabf420dd8cefccbcbf9e
Michael Wellner <michael.wellner@de.ibm.com>
2017-10-04 21:34:19 +0200

    Added methods to send a signal.

--- modules/send.js
+++ modules/send.js
@@ -1,28 +1,39 @@
 const _ = require('lodash');
 
-module.exports = (params, ow) => (intent, context = {}, outputparams = {}) => {
-  const invokeParams = {
-    name: `${_.get(params.config, 'openwhisk.package')}/core-output`,
-    blocking: true,
-    result: true,
-    params: _.assign({}, outputparams, { payload: params.payload, intent, context })
+module.exports = (params, ow) => {
+  const sendText = (intent, context = {}, outputparams = {}) => {
+    const invokeParams = {
+      name: `${_.get(params.config, 'openwhisk.package')}/core-output`,
+      blocking: true,
+      result: true,
+      params: _.assign({}, outputparams, { payload: params.payload, intent, context })
+    }
+
+    return ow.actions.invoke(invokeParams)
+      .then(result => {
+        if (result.statusCode !== 200) {
+          return Promise.reject({
+            statusCode: 503,
+            error: {
+              message: 'The core-output action did not respond with a valid result.',
+              parameters: {
+                result
+              }
+            }
+          })
+        } else {
+          _.set(params, 'payload', result.result);
+          return result.result;
+        }
+      });
   }
 
-  return ow.actions.invoke(invokeParams)
-    .then(result => {
-      if (result.statusCode !== 200) {
-        return Promise.reject({
-          statusCode: 503,
-          error: {
-            message: 'The core-output action did not respond with a valid result.',
-            parameters: {
-              result
-            }
-          }
-        })
-      } else {
-        _.set(params, 'payload', result.result);
-        return result.result;
-      }
-    });
+  const sendSignals = (signals, context = {}, outputparams = {}) => {
+    return sendText(_.join(_.concat(signals, _.get(params, 'payload.context.signals', [])), ' '), context, outputparams);
+  }
+
+  return {
+    signal: sendSignals,
+    text: sendText
+  }
 }
